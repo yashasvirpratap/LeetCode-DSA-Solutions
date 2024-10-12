daee3470b1483df153eaea89b561681866a8b18e
Yashasvi Pratap <143275662+yashasvirpratap@users.noreply.github.com>
2024-10-12 06:32:39 +0530

    Time: 20 ms (37.35%), Space: 27.1 MB (98.14%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st=0,e=s.size()-1;
+        while(st<e){
+            swap(s[st],s[e]);
+            st++;e--;
+        }
+    }
+};
