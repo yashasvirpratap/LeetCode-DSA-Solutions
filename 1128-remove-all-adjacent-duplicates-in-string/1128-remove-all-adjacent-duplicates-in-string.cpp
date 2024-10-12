a1534f9530be84485b9c751c8b27fe192ce5d41f
Yashasvi Pratap <143275662+yashasvirpratap@users.noreply.github.com>
2024-10-12 06:24:51 +0530

    Time: 15 ms (82.96%), Space: 12.4 MB (76.88%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        for (char ch : s) {
+            if (!ans.empty() && ans.back() == ch) {
+                ans.pop_back();
+            } else {
+                ans.push_back(ch);
+            }
+        }
+        return ans;
+    }
+};
