4cbadf1bb80b807e51b43f55afb7dd980eb9e401
Yashasvi Pratap <143275662+yashasvirpratap@users.noreply.github.com>
2025-01-12 22:44:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (46.85%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    char ans=0;
+       for(int i=0;i<s.size();i++){
+
+       ans^=s[i];
+    }
+        for(int i=0;i<t.size();i++){
+       ans^=t[i];
+
+    }
+    return ans;
+    }
+    
+};
